8aac9aeaef85605aad9e158b50194cb8b86d9d4e
david1234t <144063177+david1234t@users.noreply.github.com>
2023-10-05 16:01:19 -0500

    Back End

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-# Liceo-Patria
+
 # Liceo Patria, Sena.
-# 1
-# Liceo_Patria
+
--- mongodb.js
+++ mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 export const db = mongoose.connect(process.env.MON_DBURI).then (()=>{
-    console.log("😜La conexión fue realizada con éxtio ⚡✨🎉 a la bd:")
+    console.log("😜 La conexión fue realizada con éxtio ⚡✨🎉 a la bd:")
 }).catch((error) => console.error("Problemas 😒🤮😵 al conectar con la base de datos de Mongodb 😰😭😨:"))
 
 export default db;
